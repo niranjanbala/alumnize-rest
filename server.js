426797fd65cc7ca1b4a8033c10d6f146bbc16903
Niranjan Bala <Niranjan.Bala@coextrix.com>
2013-08-30 09:11:10 +0000

    modified

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ var express = require('express'),
     wines = require('./routes/employee');
  
 var app = express();
-
+app.use(express.logger());
 app.get('/employees/:id/reports', wines.findByManager);
 app.get('/employees/:id', wines.findById);
 app.get('/employees', wines.findAll);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
