a272518f32299b49dfd62208139f0f29f3a8171c
niranjan <niranjan.bala@gmail.com>
2013-09-08 23:54:05 +0530

    m

--- routes/job_posting.js
+++ routes/job_posting.js
@@ -1,10 +1,26 @@
-var MongoClient = require('mongodb').MongoClient,db;
-	MongoClient.connect("mongodb://heroku_app17819681:rhnmk5dv8hcg0ppbq35kav284l@ds043338.mongolab.com:43338/heroku_app17819681",
-	function(err, mDb) {
-		 db=mDb;
-	});
-exports.findById = function(req, res) {
+var MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server,
+    db;
+var userName="heroku_app17819681";
+var password="rhnmk5dv8hcg0ppbq35kav284l";
+var hostName="ds043338.mongolab.com"
+var port="43338";
+var dbName="heroku_app17819681";
+var mongoClient = new MongoClient(new Server(hostName, port));
 
+MongoClient.connect("mongodb://heroku_app17819681:rhnmk5dv8hcg0ppbq35kav284l@ds043338.mongolab.com:43338/heroku_app17819681", function(err, mDb) {
+    db=mDb;
+});
+exports.findById = function(req, res) {
+	    console.log(req.params);
+	    var id = parseInt(req.params.id);
+	    console.log('findById: ' + id);
+	    db.collection('employees', function(err, collection) {
+	        collection.findOne({'id': id}, function(err, item) {
+	            console.log(item);
+	            res.jsonp(item);
+	        });
+	    });
 };
 exports.postedByUser = function(req, res) {
 	    console.log(req.params);
@@ -18,7 +34,15 @@ exports.postedByUser = function(req, res) {
 	    });
 };
 exports.postJob = function(req, res) {
-
+	    console.log(req.params);
+	    var id = parseInt(req.params.id);
+	    console.log('findById: ' + id);
+	    db.collection('employees', function(err, collection) {
+	        collection.findOne({'id': id}, function(err, item) {
+	            console.log(item);
+	            res.jsonp(item);
+	        });
+	    });
 };
 exports.findAll = function(req, res) {
 
