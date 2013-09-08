be25e98b5972f0e1517e66228d2ce6ca5ed9f393
niranjan <niranjan.bala@gmail.com>
2013-09-08 23:37:33 +0530

    m

--- routes/job_posting.js
+++ routes/job_posting.js
@@ -1,5 +1,5 @@
 var MongoClient = require('mongodb').MongoClient,db;
-	MongoClient.connect("mongodb://heroku_app17819681:rhnmk5dv8hcg0ppbq35kav284l@ds043338.mongolab.com:43338/heroku_app17819681", 
+	MongoClient.connect("mongodb://heroku_app17819681:rhnmk5dv8hcg0ppbq35kav284l@ds043338.mongolab.com:43338/heroku_app17819681",
 	function(err, mDb) {
 		 db=mDb;
 	});
@@ -7,7 +7,7 @@ exports.findById = function(req, res) {
 
 };
 exports.postedByUser = function(req, res) {
-
+	res.jsonp({});
 };
 exports.postJob = function(req, res) {
 
