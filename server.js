c6b1a7a2b4cbd6ff170a7ad7609e02d03367df6f
niranjan <niranjan.bala@gmail.com>
2013-09-09 14:06:23 +0530

    fix

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ var express = require('express'),
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
+app.configure(function () {
+    app.set('port', process.env.PORT || 3000);
+    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
+    app.use(express.bodyParser()),
+    app.use(express.static(path.join(__dirname, 'views')));
+});
+
 app.get('/employees/:id/reports', wines.findByManager);
 app.get('/employees/:id', wines.findById);
 app.get('/employees', wines.findAll);
