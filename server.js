aa1faa3d9aef31a578772c42da10f3baf7dbf87d
niranjan <niranjan.bala@gmail.com>
2013-09-09 14:12:46 +0530

    fix

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    wine = require('./routes/employee'),
+    wines = require('./routes/employee'),
     jobs=require('./routes/job_posting');
-var app = express();
-app.configure(function () {
-    app.set('port', process.env.PORT || 3000);
-    app.use(express.logger('dev')); /* 'default', 'short', 'tiny', 'dev' */
-    app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, 'public')));
-});
 var app = express();
 app.use(express.logger());
 app.use(express.bodyParser());
@@ -26,9 +19,6 @@ app.get('/employees', wines.findAll);
 app.get('/employees/:id/jobPosts',jobs.postedByUser);
 app.post('/employees/:id/postJob',jobs.postJob);
 app.get('/jobPosts', jobs.findAll);
-
-var port = process.env.PORT || 5000;
-
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
 });
